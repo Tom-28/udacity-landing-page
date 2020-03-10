5e30594a9281d5ad4531e954e6abcb454e7db4f8
Unknown <tglaubitz@gmx.de>
2020-03-10 13:36:18 +0100

    feat: add element factory function + helper function

--- js/app.js
+++ js/app.js
@@ -18,12 +18,46 @@
  * 
 */
 
+const ul = document.querySelectorAll('#navbar__list');
+console.log(ul);
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
+// A HTML Element Factory Function
+const createElement = (element, attributes) => {
+    const element_without = document.createElement(element);
+    if (!(attributes === undefined)) {
+        const elements_with_attributes = createAttributes(element_without, attributes);
+        return elements_with_attributes;
+    } else {
+      return element_without;
+    }
+}
+
+// helper function to the createElement function that fills an empty element with attributes
+const createAttributes = (element, attributes) => {
+    if(!(attributes === undefined)){
+      //returning the key and value pairs of the object into an aray, that´s therefore 2dimensional
+      const attributeArray = Object.entries(attributes); 
+
+      // iterate through the several attributes possible
+      for (let i = 0; i < attributeArray.length; i++){
+        //create attribute node
+      const attr = document.createAttribute(attributeArray[i][0]);
+        //set value to attribute
+        attr.value = attributeArray[i][1];
+        // add attribute node to element
+        element.setAttributeNode(attr);
+      }
+    }
+    return element; 
+};
+
+createAttributes('<a>',{class:"listItem", href:"https://www.google.de"});
 
 
 
